b3e10e61add964381029199257c6a96fec1a99a0
Siepmann <cevin.siepmann@curemannheim.de>
2021-06-14 15:00:48 +0200

    Styling Navbar 1

--- ShaFi/src/app/app.component.ts
+++ ShaFi/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'ShaFi';
 
-  clicked: boolean=!false;
+  clicked: boolean=false;
 
   ngOnInit(): void {
   }
