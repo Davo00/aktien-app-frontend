af4a4dcd7823eb178016eeac1aa18a6c701632d4
Siepmann <cevin.siepmann@curemannheim.de>
2021-06-14 07:56:07 +0200

    Erstellen des ersten popups

--- ShaFi/src/app/app-routing.module.ts
+++ ShaFi/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DialogmainexampleComponent } from './dialogmainexample/dialogmainexample.component';
+import { DialogwindowComponent } from './dialogwindow/dialogwindow.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dialogwindow', component: DialogwindowComponent},
+  {path: 'dialog', component: DialogmainexampleComponent},
+  {path: '', component: HomeComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ ShaFi/src/app/dialogwindow/dialogwindow.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DialogwindowComponent } from './dialogwindow.component';
+
+describe('DialogwindowComponent', () => {
+  let component: DialogwindowComponent;
+  let fixture: ComponentFixture<DialogwindowComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DialogwindowComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DialogwindowComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
