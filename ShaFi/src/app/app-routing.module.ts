d40d4ed5fac259dae403f8d6e30c00f8ccce649a
Moayad_Yaghi <moayad.yaghi@sap.com>
2021-06-17 01:37:55 +0200

    group hisory page

--- ShaFi/src/app/app-routing.module.ts
+++ ShaFi/src/app/app-routing.module.ts
@@ -3,15 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { DialogmainexampleComponent } from './dialogmainexample/dialogmainexample.component';
 import { DialogwindowComponent } from './dialogwindow/dialogwindow.component';
 import { HomeComponent } from './home/home.component';
-
 import { GroupOverviewComponent } from './group-overview/group-overview.component';
+
+import { GroupHistoryComponent } from './group-history/group-history.component';
 
 const routes: Routes = [
   { path: 'group-overview', component: GroupOverviewComponent },
-  {path: 'dialogwindow', component: DialogwindowComponent},
-  {path: 'dialog', component: DialogmainexampleComponent},
-  {path: '', component: HomeComponent},
-
+  { path: 'group-history', component: GroupHistoryComponent },
+  { path: 'dialogwindow', component: DialogwindowComponent },
+  { path: 'dialog', component: DialogmainexampleComponent },
+  { path: '', component: HomeComponent },
 ];
 
 @NgModule({
