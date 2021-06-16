9f64e680439dbc90f569ee7e8f1094d93e54122b
Siepmann <cevin.siepmann@curemannheim.de>
2021-06-16 20:30:34 +0200

    chats

--- ShaFi/src/app/chatprogramm/chatprogramm.component.ts
+++ ShaFi/src/app/chatprogramm/chatprogramm.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatprogrammComponent implements OnInit {
 
+  Chats: { Absender: string; Datum:string; text: string; }[]  = [
+
+    {"Absender": "Peter", "Datum":"2021-01-18", "text": "Einkauf von Bier" },
+    {"Absender": "Ullo", "Datum":"2021-01-18", "text": "Einkauf von Bier" },
+    {"Absender": "Gabi", "Datum":"2021-01-18", "text": "Einkauf von Bier" },
+    {"Absender": "Carolina", "Datum":"2021-01-18", "text": "Einkauf von Bier" },
+    {"Absender": "Sabine", "Datum":"2021-01-18", "text": "Einkauf von Bier" },
+    /* {"name": "Gruppe1", "mitglieder": "Harald, Sabine, Peter"}, */
+    
+   
+];
+
   constructor() { }
 
   ngOnInit(): void {
