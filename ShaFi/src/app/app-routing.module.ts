c9360b69be515ad8fbc2b116b336959141bcafc1
I517970 <niklas.schaefer@sap.com>
2021-06-17 15:34:05 +0200

    Zahlungsblock

--- ShaFi/src/app/app-routing.module.ts
+++ ShaFi/src/app/app-routing.module.ts
@@ -17,4 +17,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponent = [ZahlungenComponent]
+export const routingComponent = [ZahlungenComponent, DialogwindowComponent, DialogmainexampleComponent, HomeComponent]
