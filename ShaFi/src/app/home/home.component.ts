0ef28e6864ca9c187e691ff0b8c90e75d83d75cf
I517970 <niklas.schaefer@sap.com>
2021-07-19 10:54:55 +0200

    zwischenänderung

--- ShaFi/src/app/home/home.component.ts
+++ ShaFi/src/app/home/home.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Router} from '@angular/router';
 import { ApiService } from '../services/api.service';
 
+export interface paymentType {
+  name: string,
+  amount: number,
+  inOrOut: string,
+  debtId: number
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,26 +20,7 @@ export class HomeComponent implements OnInit {
   constructor(public router: Router,
     private apiService: ApiService) { }
   Groups: any = []
-  Zahlungen: { amount: number; receiver: string }[] = [
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-    { receiver: 'Moayad', amount: 15},
-  ];
+  duePayments: paymentType[] = [];
 
   public ngOnInit(): void {
 
@@ -40,6 +28,14 @@ export class HomeComponent implements OnInit {
       this.Groups = data;
       console.log(this.Groups);
     });
+    this.apiService.getAllDebtsForUser().subscribe(data => {
+      for(let i=0; i < data.length; i++) {
+        const deadline = data[i].deadline.split("T")[0].split("-");
+        const endDate = deadline[2] + "." + deadline[1] + "." + deadline[0];
+        if(this.today > new Date(deadline[1] + " " + deadline[2] + " " + deadline[0])) {
+          const paymentObject: paymentType = {name: "", amount: data[i].amount, inOrOut: "", debtId: data[i].id};
+      }
+    }
   }
 
   public clickZahlung(){
